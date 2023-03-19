b4567d7b44b190bb4f879ed348ec6c00b6fba3cb
shashank <v.shashank77@gmail.com>
2023-03-19 23:17:31 +0530

    my file is committed fist time for mobile app

---
+++ src/main/java/com/mobapplication/_shashank/App.java
@@ -0,0 +1,15 @@
+package com.mobapplication._shashank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "my mobile application file" );
+        
+        System.out.println("my new project");
+    }
+}
