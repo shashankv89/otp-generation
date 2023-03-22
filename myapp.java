9a3eeca0cd6ad8d065cb04a3420026b443e7e2b7
shashank <v.shashank77@gmail.com>
2023-03-22 22:38:09 +0530

    changes done

---
+++ myapp.java
@@ -0,0 +1,17 @@
+public class PhoneApp{
+	
+	public static void main(String[] args){
+			//bug-101 fix changes
+			int a=10;
+			int b=20;
+		//logic pending
+			System.out.println("my phone application has been changed");
+
+	//bug-102 fix changes
+	i
+	
+		//logic pending
+	System.out.println(" on Update my phone application has been changed");
+	
+	}
+}
